6cdafd4985dd2eff9c92f0d225d132ef374771a3
shane-decatur <sdecatur20@stuy.edu>
2020-10-22 14:43:43 -0400

    modified code in main to test the methods

--- Tester.java
+++ Tester.java
@@ -1,6 +1,15 @@
 public class Tester{
   public static void main(String[] args){
-    int[] arrA = { 1, 2, 3 };
+    int[] arrA = { 100, 12, -56 };
+    int[] arrB = {2 , 4 ,9, 1, 23};
+    int[] arrC = { };
+
     System.out.println(ArrayOps.sum(arrA));
+    System.out.println(ArrayOps.sum(arrB));
+    System.out.println(ArrayOps.sum(arrC));
+
+    System.out.println(ArrayOps.largest(arrA));
+    System.out.println(ArrayOps.largest(arrB));
+    System.out.println(ArrayOps.largest(arrC));
   }
 }
