03d9b5468ca995d7b2cb2f27de26c60350f9b9c4
shane-decatur <sdecatur20@stuy.edu>
2020-10-22 15:02:50 -0400

    imported Arrays and added more tests

--- Tester.java
+++ Tester.java
@@ -1,8 +1,14 @@
+import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
     int[] arrA = { 100, 12, -56 };
     int[] arrB = {2 , 4 ,9, 1, 23};
     int[] arrC = { };
+
+    int[][]  A  =  {  {  1,  0, 12, -1 },
+                  {  7, -2,  2,  1 },
+                  { -5, -2,  2, -9 }
+                };
 
     System.out.println(ArrayOps.sum(arrA));
     System.out.println(ArrayOps.sum(arrB));
@@ -11,5 +17,8 @@ public class Tester{
     System.out.println(ArrayOps.largest(arrA));
     System.out.println(ArrayOps.largest(arrB));
     System.out.println(ArrayOps.largest(arrC));
+
+    System.out.println(Arrays.toString(ArrayOps.sumRows(A)));
+
   }
 }
