0282f1c26000dbf583a14b321216fdefe985e604
shane-decatur <sdecatur20@stuy.edu>
2020-10-25 12:11:37 -0400

    added sum() for 2d arrays and added test code

--- ArrayOps.java
+++ ArrayOps.java
@@ -31,5 +31,14 @@ public class ArrayOps{
     return bigVals;
   }
 
+  public static int sum(int[][] arr){
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++){
+      sum += sum(arr[i]);
+    }
+    return sum;
+  }
+
+
 
 }
